b709f25755103e88e7a89bbfdf67e2e58caf3eea
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-07 11:22:20 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle"
+ * main-prints "Programming is like building a multilingual puzzle"
  * return 0.
  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-		
+
 		return (0);
 }
