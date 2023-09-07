242b485db7b2b27f1ad519456564f9caea4f8b18
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-07 12:24:26 -0700

    new data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,13 @@ int main(void)
 {
 
 	int x = 20;
-	float y =3.14;
+	float y = 3.14;
 
-	printf("size of int: %ld bytes\n", sizeof(int));
-	printf("size of float: %ld bytes\n", sizeof(float));
+	printf("size of an int: %ld bytes\n", sizeof(int));
+	printf("size of a float: %ld bytes\n", sizeof(float));
+	printf("size of a long long int: %lld bytes\n", sizeof(long long int));
+	printf("size of a long int: %ld bytes\n", sizeof(long int));
+	printf("size of a char: %c bytes\n", sizeof(char));
 
 
 	return (0);
