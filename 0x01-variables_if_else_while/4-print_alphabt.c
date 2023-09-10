2208d6748bdd235a43edd78587a5b884a4096ba5
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-10 15:17:41 -0700

    exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - "excluded q and e"
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	char i;
+
+	while (i <= 'z')
+	{
+	if
+		((i != 'e') && (i != 'q'))
+
+		{
+			putchar(i);
+			putchar('\n');
+		}
+	}
+	return (0);
+}
