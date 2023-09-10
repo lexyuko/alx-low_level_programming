b26a9ce581e9c40be69246bebe032c5c88674607
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-10 11:40:50 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - "print the alphabet"
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	char i;
+
+		for
+			(i = 'a'; i <= 'z'; i++)
+			{
+				putchar(i);
+			};
+		return (0);
+
+
+
+}
