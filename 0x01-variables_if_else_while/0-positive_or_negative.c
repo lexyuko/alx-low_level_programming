5b23e5ef91f7f978a9a0d9b6b5cf12bdf28af692
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-09 23:55:39 -0700

    answer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-* main - print "c if else statement"
+/**
+* main - "c if else statement"
 * Return: Always return 0.
 */
 int main(void)
