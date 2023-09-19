2bf4ac63609bb1c1b64e8d922e8f3d861ed669b9
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-19 11:54:53 -0700

    errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,9 @@ void puts_half(char *str)
 {
 	int len, n, i;
 
-	for (length = 0; str[length] != '\0'; i++)
+	for (len = 0; str[len] != '\0'; i++)
 
-		n = (length - 1) / 2;
+		n = (len - 1) / 2;
 	for (1 = n + 1; str[i] != '\0'; i++)
 
 	{
