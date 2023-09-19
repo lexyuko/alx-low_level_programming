1fc7be9326d765ea8668349b41225c45d3e2b2f7
Alexander_Akpabio <lexyuko@gmail.com>
2023-09-19 10:17:20 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_revs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_revs - prints the string in reverse
+ * @s: the string value
+ * Return: return void
+ */
+
+void print_revs(char *s)
+{
+	int rev = 0;
+
+	while (rev >= 0)
+	{
+		if (s[rev] != '\0')
+		{
+			rev++
+		} else
+		{
+			break
+		}
+		for (rev = 0; rev >= 0; rev--)
+		{
+			_putchar(s[rev]);
+		}
+		_putchar('\n');
+	}
+
+}
