7a6663fa9e755910a36aa1bf07093f91e3d40a2c
Alexander_Akpabio <lexyuko@gmail.com>
2023-10-09 15:33:30 -0700

    completed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-		return (pnt);
+		return (pnt1);
 
 	if (new_size == 0 && pnt)
 	{
