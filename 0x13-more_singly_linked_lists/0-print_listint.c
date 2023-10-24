7ac426906502e0ef29def8d4a3fc5029b0026b2e
alexander akpabio <140617137+lexyuko@users.noreply.github.com>
2023-10-24 07:04:29 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
 
+/**
+ * print_listint - number of nodes as integers
+ * @h: header to the program
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
