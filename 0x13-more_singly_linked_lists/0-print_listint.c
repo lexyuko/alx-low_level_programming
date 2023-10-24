8ec7fbcbf27c787a111231d0c270753958f938bd
alexander akpabio <140617137+lexyuko@users.noreply.github.com>
2023-10-24 07:36:15 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tp;
-	unsigned int cntr = 0;
-
-	tp = h;
-	while (tp)
-	{
-		printf("%d\n", tp->n);
-		cntr++;
-		tp = tp->next;
-	}
-	return (cntr);
+	while (h != NULL)
+{
+    nodes++;
+    printf("%d\n", h->n);
+    h = h->next;
+}
+return (nodes);
 }
