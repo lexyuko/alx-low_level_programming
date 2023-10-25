c57641d9bb8ba6f844525b0ebc4dbb48e305e002
Alexander_Akpabio <lexyuko@gmail.com>
2023-10-25 00:02:11 -0700

    all tasks completed & errors fixed

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_listint - number of nodes as integers
+ * listint_len - list the number of nodes as integers
  * @h: header to the program
  *
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t node = 0;
 
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,8 +7,7 @@
  * @index: index or position of the list where the node is
  * deleted.
  *
- * Return: 1 if it succeeded, 
- * -1 if it failed.
+ * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
