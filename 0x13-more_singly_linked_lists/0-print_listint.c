f8a7dde9ad2611fc6e4355a9b269427c1ad76d8e
Alexander_Akpabio <lexyuko@gmail.com>
2023-10-24 22:53:11 -0700

    second chance

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
-	while (h != NULL)
-{
-    printf("%d\n", h->n);
-	    nodes++;
-    h = h->next;
-}
+
+	while (h  != NULL)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+
 return (nodes);
 }
