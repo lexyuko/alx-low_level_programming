c199bd4905036c9ce778db9a1e148436a1c888eb
alexander akpabio <140617137+lexyuko@users.noreply.github.com>
2023-10-27 06:49:37 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (x >> 0)
+	if (n >> 0)
 	{
-		if (x >> 1)
+		if (n >> 1)
 			print_binary(x >> 1);
-		_putchar((x & 1) + '0');
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
